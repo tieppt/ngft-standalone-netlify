e4664d6c7a921c358744e3f996a44c8cbb25f255
Tiep Phan <tieppt12@gmail.com>
2022-07-02 19:17:21 +0700

    add netlify config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
 })
 export class AppComponent {
   title = 'ngft-standalone-netlify';
